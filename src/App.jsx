d28e1b70365b6265304386b2ced4a23f024db89f
nicotinovi <basmanovsemens@gmail.com>
2026-05-19 02:59:03 +0800

    part1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Root from './app/root'
+
+const router = createBrowserRouter([{ path: '/', element: <Root /> }])
+
+function App() {
+	return <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/app/root.jsx
@@ -0,0 +1,18 @@
+function Root() {
+	return (
+		<div id="main">
+			<div id="menu">
+				<nav>
+					<a>Products</a>
+					<a>Sellers</a>
+				</nav>
+			</div>
+			<div id="main_page">
+				<h2>This is my first Redux app!</h2>
+				<hr></hr>
+			</div>
+		</div>
+	)
+}
+
+export default Root
